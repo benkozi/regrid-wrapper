7c78305c9ffa172cd55dba0569293b5c5a7261e1
Ben Koziol <bkoziol@redlineperf.com>
2024-12-23 11:38:42 -0700

    added rave to rrfs 25km test

--- src/test/test_concrete/test_rave_rrfs_25km.py
+++ src/test/test_concrete/test_rave_rrfs_25km.py
@@ -1,14 +1,21 @@
 from pathlib import Path
 
+import numpy as np
+import pytest
+from pip._internal import resolution
+
 from regrid_wrapper.concrete.rave_to_rrfs import RaveToRrfs
+from regrid_wrapper.context.comm import COMM
 from regrid_wrapper.model.spec import (
     GenerateWeightFileSpec,
 )
 
 from regrid_wrapper.strategy.core import RegridProcessor
+import xarray as xr
 
 
-def test(bin_dir: Path, tmp_path_shared: Path) -> None:
+@pytest.mark.skip("dev only")
+def test_dev(bin_dir: Path, tmp_path_shared: Path) -> None:
     spec = GenerateWeightFileSpec(
         src_path=bin_dir / "RAVE/grid_in.nc",
         dst_path=bin_dir / "RRFS_CONUS_25km/ds_out_base.nc",
@@ -20,3 +27,42 @@ def test(bin_dir: Path, tmp_path_shared: Path) -> None:
     op = RaveToRrfs(spec=spec)
     processor = RegridProcessor(operation=op)
     processor.execute()
+
+
+@pytest.mark.mpi
+def test(tmp_path_shared: Path) -> None:
+    src_grid = tmp_path_shared / "src_grid.nc"
+    dst_grid = tmp_path_shared / "dst_grid.nc"
+    weights = tmp_path_shared / "weights.nc"
+
+    if COMM.rank == 0:
+        lon = np.linspace(230, 300, 71)
+        lonc = np.hstack((lon - 0.5, [lon[-1] + 0.5]))
+        lat = np.linspace(25, 50, 26)
+        latc = np.hstack((lat - 0.5, [lat[-1] + 0.5]))
+
+        lon, lat = np.meshgrid(lon, lat)
+        lonc, latc = np.meshgrid(lonc, latc)
+
+        ds = xr.Dataset()
+        ds["grid_lont"] = xr.DataArray(lon, dims=["lat", "lon"])
+        ds["grid_latt"] = xr.DataArray(lat, dims=["lat", "lon"])
+        ds["grid_lon"] = xr.DataArray(lonc, dims=["latc", "lonc"])
+        ds["grid_lat"] = xr.DataArray(latc, dims=["latc", "lonc"])
+
+        ds.to_netcdf(src_grid)
+        ds.to_netcdf(dst_grid)
+
+    spec = GenerateWeightFileSpec(
+        src_path=src_grid,
+        dst_path=dst_grid,
+        output_weight_filename=weights,
+        esmpy_debug=True,
+        name="tester",
+        machine="hera",
+    )
+    op = RaveToRrfs(spec=spec)
+    processor = RegridProcessor(operation=op)
+    processor.execute()
+
+    assert Path(weights).exists()
--- src/test/test_esmpy.py
+++ src/test/test_esmpy.py
@@ -1,4 +1,48 @@
+import esmpy
+import numpy as np
+
+DEG2RAD = 3.141592653589793 / 180.0
+
+
 def test() -> None:
     import esmpy
 
     assert esmpy is not None
+
+
+def test_xy_ordering_behavior() -> None:
+    x, y = [0, 1]
+    lat = [33, 44, 55]
+    lon = [-104, -80, -60]  # tdk: test with unwrapped
+
+    lon, lat = np.meshgrid(lon, lat)
+
+    grid = esmpy.Grid(
+        np.array(lat.shape),
+        coord_sys=esmpy.CoordSys.SPH_DEG,
+        staggerloc=[esmpy.StaggerLoc.CENTER],
+    )
+
+    x_coords = grid.get_coords(x, staggerloc=esmpy.StaggerLoc.CENTER)
+    x_coords[:] = lon
+
+    y_coords = grid.get_coords(y, staggerloc=esmpy.StaggerLoc.CENTER)
+    y_coords[:] = lat
+
+    src_field = esmpy.Field(grid, name="src")
+    src_field.data[:] = 2.0 + np.cos(DEG2RAD * lon) ** 2 * np.cos(
+        2.0 * DEG2RAD * (90.0 - lat)
+    )
+
+    dst_field = esmpy.Field(grid, name="dst")
+
+    regrid = esmpy.Regrid(
+        src_field, dst_field, regrid_method=esmpy.RegridMethod.BILINEAR
+    )
+
+    dst_field = regrid(src_field, dst_field)
+
+    print(src_field.data)
+    print(dst_field.data)
+    print(src_field.data - dst_field.data)
+    print(np.mean(dst_field.data))
