444e9edb50e6818e789bdc508dd18d58a905ba49
Ben Koziol <bkoziol@redlineperf.com>
2025-01-08 05:59:13 -0700

    add test script

---
+++ script/hera/test-rrfs-25km.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+cd ~/l/scratch/sandbox/regrid-wrapper || exit
+rm -rf foo-test
+
+export PYTHONPATH=$(pwd -LP)/src
+export REGRID_WRAPPER_LOG_DIR=.
+
+~/l/scratch/miniconda/envs/regrid-wrapper/bin/python ./src/regrid_wrapper/hydra/task_prep.py
+
+cd foo-test/logs || exit
+sbatch ../main-job.sh
+squeue -u Benjamin.Koziol -i 5
