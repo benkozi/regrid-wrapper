d9b1678f576f0ed47366e2d11eda34784a95b78e
Ben Koziol <bkoziol@redlineperf.com>
2025-01-08 10:33:58 -0700

    test whole shebang

--- script/hera/run-smoke-dust-regrid.sh
+++ script/hera/run-smoke-dust-regrid.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
+RUNDIR=smoke-dust-fixed-files
+
 cd ~/l/scratch/sandbox/regrid-wrapper || exit
 git pull
-rm -rf smoke-dust-fixed-files
+rm -rf ${RUNDIR}
 
 export PYTHONPATH=$(pwd -LP)/src
 export REGRID_WRAPPER_LOG_DIR=.
 
 ~/l/scratch/miniconda/envs/regrid-wrapper/bin/python ./src/regrid_wrapper/hydra/task_prep.py
 
-cd foo-test/logs || exit
+cd ${RUNDIR}/logs || exit
 sbatch ../main-job.sh
 squeue -u Benjamin.Koziol -i 5
