14776b58974f2a2d7d92476f8a6a8bcad7bd0189
Ben Koziol <bkoziol@redlineperf.com>
2025-01-08 11:40:26 -0700

    readme stuff

--- src/test/test_numpy.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def test_moveaxis() -> None:
-    arr = np.array([[1, 2, 3], [4, 5, 6]])
-    # print(arr)
-    moved = np.moveaxis(arr, 0, 1)
-    # print(moved.tolist())
-    assert np.array_equal(moved, np.array([[1, 4], [2, 5], [3, 6]]))
