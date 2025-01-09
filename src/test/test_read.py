9a67459b6f641cae935ba1f8266af04d6e692f2b
Ben Koziol <bkoziol@redlineperf.com>
2025-01-09 12:42:45 -0700

    test read

--- src/test/test_read.py
+++ src/test/test_read.py
@@ -2,38 +2,38 @@ from pathlib import Path
 
 import esmpy
 
-from regrid_wrapper.esmpy.field_wrapper import GridWrapper, NcToGrid, GridSpec
 
-
-def _create_grid_wrapper_(path: Path) -> GridWrapper:
-    nc2grid = NcToGrid(
-        path=path,
-        spec=GridSpec(
-            x_center="grid_lont",
-            y_center="grid_latt",
-            x_dim=("grid_xt",),
-            y_dim=("grid_yt",),
-            x_corner="grid_lon",
-            y_corner="grid_lat",
-            x_corner_dim=("grid_x",),
-            y_corner_dim=("grid_y",),
-        ),
-    )
-    gwrap = nc2grid.create_grid_wrapper()
-    return gwrap
+# def _create_grid_wrapper_(path: Path) -> GridWrapper:
+#     nc2grid = NcToGrid(
+#         path=path,
+#         spec=GridSpec(
+#             x_center="grid_lont",
+#             y_center="grid_latt",
+#             x_dim=("grid_xt",),
+#             y_dim=("grid_yt",),
+#             x_corner="grid_lon",
+#             y_corner="grid_lat",
+#             x_corner_dim=("grid_x",),
+#             y_corner_dim=("grid_y",),
+#         ),
+#     )
+#     gwrap = nc2grid.create_grid_wrapper()
+#     return gwrap
 
 
 def run() -> None:
 
-    src_gwrap = _create_grid_wrapper_(
-        "/scratch2/NAGAPE/epic/SRW-AQM_DATA/fix_smoke/RRFS_NA_3km/grid_in.nc"
-    )
-    dst_gwrap = _create_grid_wrapper_(
-        "/scratch2/NAGAPE/epic/Ben.Koziol/tmp-smoke-fix-dir/RRFS_NA_13km/ds_out_base.nc"
-    )
+    # src_gwrap = _create_grid_wrapper_(
+    #     "/scratch2/NAGAPE/epic/SRW-AQM_DATA/fix_smoke/RRFS_NA_3km/grid_in.nc"
+    # )
+    # dst_gwrap = _create_grid_wrapper_(
+    #     "/scratch2/NAGAPE/epic/Ben.Koziol/tmp-smoke-fix-dir/RRFS_NA_13km/ds_out_base.nc"
+    # )
 
-    src_field = esmpy.Field(src_gwrap.value, name="src")
-    dst_field = esmpy.Field(dst_gwrap.value, name="dst")
+    grid = esmpy.Grid([2, 2])
+
+    src_field = esmpy.Field(grid, name="src")
+    dst_field = esmpy.Field(grid, name="dst")
 
     # regrid_method = esmpy.RegridMethod.CONSERVE
     # regridder = esmpy.Regrid(
@@ -49,12 +49,12 @@ def run() -> None:
     _ = esmpy.RegridFromFile(
         src_field,
         dst_field,
-        # str(
-        #     "/scratch2/NAGAPE/epic/Ben.Koziol/tmp-smoke-fix-dir/RRFS_NA_13km/weight_file.nc"
-        # ),
         str(
-            "/scratch2/NAGAPE/epic/Ben.Koziol/tmp-smoke-fix-dir/RRFS_CONUS_25km/weight_file.nc"
+            "/scratch2/NAGAPE/epic/Ben.Koziol/tmp-smoke-fix-dir/RRFS_NA_13km/weight_file.nc"
         ),
+        # str(
+        #     "/scratch2/NAGAPE/epic/Ben.Koziol/tmp-smoke-fix-dir/RRFS_CONUS_25km/weight_file.nc"
+        # ),
     )
 
 
