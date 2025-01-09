2df6ed37540bedf787b70d6aa79ee8ffdd1b0c4a
Ben Koziol <bkoziol@redlineperf.com>
2025-01-09 12:56:04 -0700

    test read

--- script/hera/run-smoke-dust-regrid.sh
+++ script/hera/run-smoke-dust-regrid.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 RUNDIR=smoke-dust-fixed-files
-CONDAENV=~/l/scratch/miniconda/envs/regrid-wrapper
+#CONDAENV=~/l/scratch/miniconda/envs/regrid-wrapper
+CONDAENV=~/miniconda3/envs/tmp-uni
 
 cd ~/l/scratch/sandbox/regrid-wrapper || exit
 git pull
