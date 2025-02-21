775aa310ade5a34c0d546ba405a703052503f7b1
Ben Koziol <bkoziol@redlineperf.com>
2025-02-21 11:13:09 -0700

    test esmpy mpas on hera [ci skip]

--- script/mpas/job-regrid.sh
+++ script/mpas/job-regrid.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
-##
-##SBATCH --job-name=mpas-regrid
-##SBATCH --account=epic
-##SBATCH --qos=batch
-##SBATCH --partition=batch
-##SBATCH -t 00:05:00
-##SBATCH --output=/home/Benjamin.Koziol/htmp/%x_%j.out
-##SBATCH --error=/home/Benjamin.Koziol/htmp/%x_%j.err
-##SBATCH --nodes=1
-##SBATCH --ntasks-per-node=1  # Assuming 24 cores per node, utilize them fully
-##SBATCH --ntasks=1  # Total tasks should be nodes * tasks-per-node
+#
+#SBATCH --job-name=mpas-regrid
+#SBATCH --account=epic
+#SBATCH --qos=batch
+#SBATCH --partition=batch
+#SBATCH -t 00:05:00
+#SBATCH --output=/home/Benjamin.Koziol/htmp/%x_%j.out
+#SBATCH --error=/home/Benjamin.Koziol/htmp/%x_%j.err
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1  # Assuming 24 cores per node, utilize them fully
+#SBATCH --ntasks=1  # Total tasks should be nodes * tasks-per-node
 
 set -e
 
