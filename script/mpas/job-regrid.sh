b30d455ca58272f16f9451696ef8aea627347237
Ben Koziol <bkoziol@redlineperf.com>
2025-02-21 11:10:56 -0700

    test esmpy mpas on hera [ci skip]

--- script/mpas/job-regrid.sh
+++ script/mpas/job-regrid.sh
@@ -3,7 +3,7 @@
 ##SBATCH --job-name=mpas-regrid
 ##SBATCH --account=epic
 ##SBATCH --qos=batch
-##SBATCH --partition=bigmem
+##SBATCH --partition=batch
 ##SBATCH -t 00:05:00
 ##SBATCH --output=/home/Benjamin.Koziol/htmp/%x_%j.out
 ##SBATCH --error=/home/Benjamin.Koziol/htmp/%x_%j.err
