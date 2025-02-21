17411042ef067629d6e06d6f1d8875f4426cc5ed
Ben Koziol <bkoziol@redlineperf.com>
2025-02-21 12:19:17 -0700

    test esmpy mpas on hera [ci skip]

--- script/mpas/regrid.py
+++ script/mpas/regrid.py
@@ -25,7 +25,7 @@ class RegridProcessor(BaseModel):
         esmpy.Manager(debug=True)
 
         if self.context.rank == 0:
-            _LOGGER("writing mpas scrip grid")
+            _LOGGER.log("writing mpas scrip grid")
             mpas_desc = MpasCellMeshDescriptor(
                 str(self.context.dst_path), "na15km.init"
             )
@@ -48,10 +48,10 @@ class RegridProcessor(BaseModel):
         )
         src_gwrap = nc2grid.create_grid_wrapper()
 
-        _LOGGER("create destination mesh")
+        _LOGGER.log("create destination mesh")
         dst_mesh = esmpy.Mesh(filename=str(scrip_path), filetype=esmpy.FileFormat.SCRIP)
 
-        _LOGGER("create regridder")
+        _LOGGER.log("create regridder")
         src_field = esmpy.Field(src_gwrap.value, name="src")
         dst_field = esmpy.Field(dst_mesh, name="dst")
         regridder = esmpy.Regrid(
