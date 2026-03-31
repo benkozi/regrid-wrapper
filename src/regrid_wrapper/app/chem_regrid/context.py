64746162dbec2cfeaf968e0e604b2f82a531befe
Ben Koziol <bkoziol@redlineperf.com>
2026-03-31 14:48:41 -0500

    [ci skip] chem_regrid cli

--- src/regrid_wrapper/app/chem_regrid/context.py
+++ src/regrid_wrapper/app/chem_regrid/context.py
@@ -1,8 +1,10 @@
 from abc import ABC
+from enum import unique, StrEnum
+from functools import cached_property
 from pathlib import Path
 
 import yaml
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, computed_field
 
 
 class RwBaseModel(ABC, BaseModel):
@@ -19,5 +21,43 @@ class RwBaseModel(ABC, BaseModel):
         return cls.from_yaml(yaml_data)
 
 
+@unique
+class DatasetName(StrEnum):
+    RAVE = "rave"
+
+
 class ChemRegridContext(RwBaseModel):
-    path: Path
+    dataset_name: DatasetName
+    workdir: Path
+    input_dir: Path
+    output_dir: Path
+    weight_dir: Path
+    cycle: str = Field(pattern=r"^\d{10}$")  # Validates YYYYMMDDHH format
+    mesh_name: str
+    scrip_path: Path | None
+    dst_path: Path | None
+    ebb_dcycle: int
+
+    def finalize_paths(self):
+        """Replicates the path logic from your original script."""
+        if str(self.scrip_path) == ".":  # Path("") often resolves to "."
+            self.scrip_path = self.workdir / f"mpas_{self.dataset_name}-{self.mesh_name}_scrip.nc"
+
+        if str(self.dst_path) == ".":
+            self.dst_path = self.workdir / "init.nc"
+
+    @cached_property
+    def rw_scrip_path(self) -> Path:
+        if self.scrip_path is None:
+            return self.workdir / f"mpas_{self.dataset_name.value}-{self.mesh_name}_scrip.nc"
+        return self.scrip_path
+
+    @cached_property
+    def rw_dst_path(self) -> Path:
+        if self.dst_path is None:
+            return self.workdir / "init.nc"
+        return self.dst_path
+
+    @cached_property
+    def rw_desc_stats_out(self) -> Path:
+        return self.workdir / f"desc_stats-{self.cycle}.csv"
