6458ea50342c40da38b93d4973e2a0d7ce7a2c6b
Ben Koziol <bkoziol@redlineperf.com>
2026-03-31 13:53:06 -0500

    [ci skip] chem_regrid cli

--- src/regrid_wrapper/app/chem_regrid/context.py
+++ src/regrid_wrapper/app/chem_regrid/context.py
@@ -1,12 +1,22 @@
 from abc import ABC
 from pathlib import Path
 
+import yaml
 from pydantic import BaseModel
 
 
 class RwBaseModel(ABC, BaseModel):
     model_config = {"frozen": True}
 
+    @classmethod
+    def from_yaml(cls, data: dict) -> "RwBaseModel":
+        return cls.model_validate(data)
+
+    @classmethod
+    def from_yaml_file(cls, path: Path) -> "RwBaseModel":
+        string_data = path.read_text()
+        yaml_data = yaml.safe_load(string_data)
+        return cls.from_yaml(yaml_data)
 
 class ChemRegridContext(RwBaseModel):
     path: Path
