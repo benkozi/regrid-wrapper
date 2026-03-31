da03f762f739a140eaa2ccfeee441ae226be4f19
Ben Koziol <bkoziol@redlineperf.com>
2026-03-31 09:03:55 -0500

    add pydantic-settings to spack env [ci skip]

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -4,14 +4,14 @@ set -eux
 
 docker build \
   --platform linux/amd64 \
-  -t deckyfre/regrid-wrapper-spack:0.0.5 \
+  -t deckyfre/regrid-wrapper-spack:0.0.6 \
   -t deckyfre/regrid-wrapper-spack:latest \
   -f Dockerfile-Spack \
   .
 
 docker build \
   --platform linux/amd64 \
-  -t deckyfre/regrid-wrapper-ci:0.0.3 \
+  -t deckyfre/regrid-wrapper-ci:0.0.4 \
   -t deckyfre/regrid-wrapper-ci:latest \
   -f Dockerfile-Spack-CI \
   .
--- docker/docker-push.sh
+++ docker/docker-push.sh
@@ -2,8 +2,8 @@
 
 set -eux
 
-docker push deckyfre/regrid-wrapper-spack:0.0.5
+docker push deckyfre/regrid-wrapper-spack:0.0.6
 docker push deckyfre/regrid-wrapper-spack:latest
 
-docker push deckyfre/regrid-wrapper-ci:0.0.3
+docker push deckyfre/regrid-wrapper-ci:0.0.4
 docker push deckyfre/regrid-wrapper-ci:latest
