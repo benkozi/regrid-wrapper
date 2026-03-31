5649b4da068e2df821f58750b5f68149f7c15b22
Ben Koziol <bkoziol@redlineperf.com>
2026-03-31 14:03:11 -0500

    [ci skip] chem_regrid cli

--- src/regrid_wrapper/app/rw_cli.py
+++ src/regrid_wrapper/app/rw_cli.py
@@ -49,6 +49,16 @@ def main() -> None:
         "--root-key", type=str, default="rw-verify", help="If provided, use this key when extracting the root configuration"
     )
 
+    # chem_regrid sub-command
+    parser_chem_regrid = subparsers.add_parser("chem-regrid", help="Run chem-regrid")
+    parser_chem_regrid.add_argument("--yaml-path", type=str, required=True, help="Path to YAML file containing the configuration's root key")
+    parser_chem_regrid.add_argument("--root-key", type=str, default="rw-chem-regrid", help="If provided, use this key when extracting the root configuration")
+    parser_chem_regrid.add_argument(
+        '--override',
+        nargs='+',
+        help="Input arbitrary strings (e.g. --override key1:nest=val1 key2=val2)"
+    )
+
     args = parser.parse_args()
 
     if args.command == "mpas-to-ugrid":
