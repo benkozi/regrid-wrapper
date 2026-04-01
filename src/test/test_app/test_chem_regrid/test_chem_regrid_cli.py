5fb7e6d95a7e3a10cde7fb7724972c52e55ca876
Ben Koziol <bkoziol@redlineperf.com>
2026-04-01 11:22:10 -0500

    [ci skip] test_chem_regrid_cli.py

--- src/test/test_app/test_chem_regrid/test_chem_regrid_cli.py
+++ src/test/test_app/test_chem_regrid/test_chem_regrid_cli.py
@@ -1,19 +1,25 @@
-import pytest
 from pathlib import Path
+
+import pytest
+from _pytest.fixtures import FixtureRequest
 from pydantic import BaseModel
+
 from regrid_wrapper.app.chem_regrid.context import ChemRegridContext, DatasetName
+from test.conftest import TEST_LOGGER
+
 
 class ContextForTest(BaseModel):
     root_path: Path
     use_scrip: bool
     use_dst: bool
 
+
 def create_chem_regrid_context(test_context: ContextForTest) -> ChemRegridContext:
     """
     Generate a ChemRegridContext object.
     If a field has a None type, it should generate both with the provided value and without it.
     """
-    
+
     # Required fields with example values
     base_params = {
         "dataset_name": DatasetName.RAVE,
@@ -30,42 +36,42 @@ def create_chem_regrid_context(test_context: ContextForTest) -> ChemRegridContex
     params = base_params.copy()
     params["scrip_path"] = test_context.root_path / "scrip.nc" if test_context.use_scrip else None
     params["dst_path"] = test_context.root_path / "dst.nc" if test_context.use_dst else None
-    
-    return ChemRegridContext(**params)
 
-@pytest.fixture(
-    params=[
-        (True, True),
-        (True, False),
-        (False, True),
-        (False, False),
-    ],
-    ids=[
-        "scrip-dst",
-        "scrip-no_dst",
-        "no_scrip-dst",
-        "no_scrip-no_dst",
-    ]
-)
-def context_for_test(request) -> ContextForTest:
-    use_scrip, use_dst = request.param
-    return ContextForTest(
-        root_path=Path("/tmp"),
-        use_scrip=use_scrip,
-        use_dst=use_dst
-    )
+    return ChemRegridContext.model_validate(params)
 
-def test_generate_chem_regrid_context(context_for_test: ContextForTest):
-    context = create_chem_regrid_context(context_for_test)
-    
-    assert isinstance(context, ChemRegridContext)
-    
+
+@pytest.fixture(params=[True, False])
+def use_scrip_path(request: FixtureRequest) -> bool:
+    return request.param
+
+
+@pytest.fixture(params=[True, False])
+def use_dst_path(request: FixtureRequest) -> bool:
+    return request.param
+
+
+@pytest.fixture()
+def context_for_test(use_scrip_path: bool, use_dst_path: bool, tmp_path_shared: Path) -> ContextForTest:
+    return ContextForTest(root_path=tmp_path_shared, use_scrip=use_scrip_path, use_dst=use_dst_path)
+
+
+@pytest.fixture
+def chem_regrid_context(context_for_test: ContextForTest) -> ChemRegridContext:
+    TEST_LOGGER.debug(f"{context_for_test=}")
+    return create_chem_regrid_context(context_for_test)
+
+
+def test_generate_chem_regrid_context(chem_regrid_context: ChemRegridContext, context_for_test: ContextForTest) -> None:
+    TEST_LOGGER.debug(f"{chem_regrid_context=}")
+
+    assert isinstance(chem_regrid_context, ChemRegridContext)
+
     if context_for_test.use_scrip:
-        assert context.scrip_path == context_for_test.root_path / "scrip.nc"
+        assert chem_regrid_context.scrip_path == context_for_test.root_path / "scrip.nc"
     else:
-        assert context.scrip_path is None
-        
+        assert chem_regrid_context.scrip_path is None
+
     if context_for_test.use_dst:
-        assert context.dst_path == context_for_test.root_path / "dst.nc"
+        assert chem_regrid_context.dst_path == context_for_test.root_path / "dst.nc"
     else:
-        assert context.dst_path is None
+        assert chem_regrid_context.dst_path is None
