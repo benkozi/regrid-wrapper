b6c2b1774b721465f0f3e5a77d38d6f6673e0fbd
Ben Koziol <bkoziol@redlineperf.com>
2026-04-09 12:11:04 -0600

    [ci skip] bring in changes from rrfs-workflow

--- script/spack-stack/env.sh
+++ script/spack-stack/env.sh
@@ -18,5 +18,6 @@ spack_stack_dirname=spack-stack-v2
 
 site=ursa
 sandbox=/scratch3/NCEPDEV/stmp/Benjamin.Koziol/sandbox
-upstream="--upstream /contrib/spack-stack/spack-stack-2.1.0/envs/ue-oneapi-2025.3.1/install"
+upstream_env=/contrib/spack-stack/spack-stack-2.1.0/envs/ue-oneapi-2025.3.1/install
+upstream="--upstream ${upstream_env}"
 spack_stack_branch=""
