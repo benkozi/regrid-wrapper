7feb280497c70e55f1cab7a067307749a87fc1a2
Ben Koziol <bkoziol@redlineperf.com>
2026-04-23 08:49:35 -0600

    [ci skip] move context

--- src/regrid_wrapper/app/chem_regrid/chem_regrid_impl.py
+++ src/regrid_wrapper/app/chem_regrid/chem_regrid_impl.py
@@ -12,7 +12,10 @@ from pydantic import BaseModel
 
 from regrid_wrapper.app.chem_regrid.chem_regrid_context import CR_LOGGER, ChemRegridContext
 from regrid_wrapper.app.chem_regrid.dataset.config.model import InterpMethod
-from regrid_wrapper.app.chem_regrid.dataset.dataset_regrid_context import AbstractDatasetRegridContext, get_regrid_context_class
+from regrid_wrapper.app.chem_regrid.dataset.context.dataset_regrid_context import (
+    AbstractDatasetRegridContext,
+    get_regrid_context_class,
+)
 from regrid_wrapper.app.chem_regrid.dataset.src_field import SrcField
 from regrid_wrapper.context.comm import COMM, reconcile_bounds
 from regrid_wrapper.esmpy.field_wrapper import (
